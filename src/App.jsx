45db38186b7a89bd93e89d90652ca94fb9ddd11f
Ibrahim Itani <I.itani2002@gmail.com>
2024-08-10 13:04:59 +0300

    finish SearchForm component design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import SearchForm from "./SearchForm";
+import SearchForm from "./SearchForm.jsx";
 function App() {
   return (
     <>
-        <div className="shadow-lg bg-action2 w-full flex justify-center items-center py-5 ">
+        <div className="shadow-xl bg-action2 w-full flex justify-center items-center py-5 ">
           <h1 className="text-4xl h-fit font-bold">Ja<span className="text-action">mmm</span>ing</h1>
         </div>
-        <div id="main body" className="bg-second-back h-screen">
+        <div id="main body" className="bg-second-back h-screen flex">
+          <div className="w-3/6 mx-auto mt-14">
           <SearchForm/>
+          </div>
         </div>
     </>
   );
--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,3 +1,18 @@
+import {useState} from "react";
 export default function SearchForm(){
-    
+    const [searchQuery, setSearchQuery]= useState('');
+    const handleInputChange = (Event)=>{
+        setSearchQuery(Event.target.value);
+    }
+    const handleSearch = ()=>{
+        console.log('Searching for: ',searchQuery);
+    }
+    return(
+        <>
+        <form action="#" onSubmit={handleSearch} className="flex flex-col"> 
+            <input className="text-action2 shadow-lg py-3 text-center font-bold" id="songName" required type="text" onChange={handleInputChange} placeholder="Enter A Song Title"/>
+            <button type="submit" className="bg-main-back text-white mt-7 w-2/4 py-2 rounded-full mx-auto font-semibold shadow-md text-lg hover:scale-105">Search</button>
+        </form>
+        </>
+    );
 }

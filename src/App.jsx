b6715e47ecb2b404d7699f94e8406265c038c38e
Ibrahim Itani <I.itani2002@gmail.com>
2024-08-10 12:09:13 +0300

    remove preset styles to use tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 function App() {
   return (
-    <div className="inhe">
+    <>
       <header>
         <div className="bg-main-back w-full">
           <h1>Ja<span className="text-action">mmm</span>ing</h1>
         </div>
       </header>
-    </div>
+    </>
   );
 }
 

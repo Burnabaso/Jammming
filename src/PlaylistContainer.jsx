93ad438a9b9ccc2edd1d4f11d489e6f8390cab66
Ibrahim Itani <I.itani2002@gmail.com>
2024-08-12 15:58:29 +0300

    add dark mode toggle functionality

--- src/PlaylistContainer.jsx
+++ src/PlaylistContainer.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
 export default function PlaylistContainer(){
+    const [searchQuery, setSearchQuery]= useState('');
+    const handleInputChange = (Event)=>{
+        setSearchQuery(Event.target.value);
+    }
+    const handleSubmit =  ()=>{
+       
+    }
     return(
         <>
         <h2 className="text-5xl text-action font-semibold text-center my-4">Playlist</h2>
@@ -14,8 +22,8 @@ export default function PlaylistContainer(){
             <i className="fa-solid fa-minus scale-150 text-action font-bold"></i>
             </div>
         </div>
-        <form action="#" onSubmit="" className="my-7 flex flex-col w-3/5 mx-auto"> 
-            <input className="text-action2 border-2 border-action shadow-lg py-3 text-center font-bold focus:outline-none" id="songName" required type="text" value="" onChange="" placeholder="Name Playlist"/>
+        <form action="#" onSubmit={handleSubmit} className="my-7 flex flex-col w-3/5 mx-auto"> 
+            <input className="text-action2 border-2 border-action shadow-lg py-3 text-center font-bold focus:outline-none" id="songName" required type="text" value={searchQuery} onChange={handleInputChange} placeholder="Name Playlist"/>
             <button type="submit" className="bg-main-back text-white mt-7 w-2/4 py-2 rounded-full mx-auto font-semibold shadow-md text-lg hover:scale-105">Add to Spotify</button>
         </form>
         </>
